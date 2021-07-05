7f22f993e0ad4b10de5c0d52e0affcd2ba77dbce
Vitaliy Vaskin <vital0101@yandex.ru>
2021-07-05 16:52:05 +0300

    начало работы

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+//const profileInfo = document.querySelector(".profile__info");
+//console.log(profileInfo);
+const profileEditButton = document.querySelector(".profile__edit-button");
+console.log(profileEditButton);
+const popUp = document.querySelector(".popup");
+//console.log(popUp);
+// popUp.addEventListener("click");
 
+function openProfile() {
+  popUp.setAttribute("display", "block");
+}
+
+profileEditButton.addEventListener("click", openProfile);
+
+//popUp.hasAttribute("display");
