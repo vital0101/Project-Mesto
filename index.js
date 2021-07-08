b9b0d534df231f5102b377ed5f8e6cacfb33e0d1
Vitaliy Vaskin <vital0101@yandex.ru>
2021-07-08 18:00:26 +0300

    в js файле изменил способ вызова попапа для кнопки edit-button

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const profileInfo = document.querySelector(".profile__info");
+//const profileInfo = document.querySelector(".profile__info");
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profileAddButton = document.querySelector(".profile__add-button");
 const popupToggle = document.querySelector(".popup__toggle");
 const popUp = document.querySelector(".popup");
 
-function openProfile() {
-  popUp.setAttribute("style", "display:block");
-}
-profileEditButton.addEventListener("click", openProfile);
+// function openProfile() {
+//   // popUp.setAttribute("style", "display:block");
+// }
+// profileEditButton.addEventListener("click", openProfile);
 
-function closeProfile() {
-  popUp.setAttribute("style", "display:none");
-}
-popupToggle.addEventListener("click", closeProfile);
+profileEditButton.onclick = function () {
+  popUp.classList.add("popup_opened");
+  console.log("Меня нажали!");
+};
+
+popupToggle.onclick = function () {
+  popUp.classList.remove("popup_opened");
+  console.log("Меня тоже нажали!");
+};
